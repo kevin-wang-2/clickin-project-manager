9f38440214b88aaaccec6c9b1b95be9c09d5792a
ByteDance <enniefeng@gmail.com>
2026-05-25 13:22:11 +0800

    temp: save local changes before rebase

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: '/app',
   env: { NEXT_PUBLIC_BASE_PATH: '/app' },
-  allowedDevOrigins: ['127.0.0.1'],
+  allowedDevOrigins: ['192.168.71.96'],
 };
 
 export default nextConfig;
